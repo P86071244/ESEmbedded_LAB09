e86e0d4cd1e8c581512c4d31d848287524eb2ebd
P86071244 <P86071244@gs.ncku.edu.tw>
2019-06-12 01:04:12 +0800

    1

--- malloc/main.c
+++ malloc/main.c
@@ -4,9 +4,27 @@
 #include <stdlib.h>
 #define HEAP_MAX (64 * 1024) //64 KB
 
+void *_sbrk(int incr)
+{
+	extern uint8_t _mybss_vma_end; //Defined by the linker script
+	static uint8_t *heap_end = NULL;
+	uint8_t *prev_heap_end;
+
+	if (heap_end == NULL)
+		heap_end = &_mybss_vma_end;
+
+	prev_heap_end = heap_end;
+	if (heap_end + incr > &_mybss_vma_end + HEAP_MAX)
+		return (void *)-1;
+
+	heap_end += incr;
+	return (void *)prev_heap_end;
+}
+
 void init_usart1(void)
 {
-	//PB6 PB7
+	// PB6: USART1_Tx 
+	// PB7: USART1_Rx
 
 	//RCC EN GPIOB
 	//??????
@@ -14,8 +32,8 @@ void init_usart1(void)
 
 
 	//GPIO Configurations
-	//??????
 	//MODER  => General purpose output mode
+	// MODER 10: Alternate function mode
 	SET_BIT(GPIO_BASE(GPIO_PORTB) + GPIOx_MODER_OFFSET, MODERy_1_BIT(6)); //PB6
 	CLEAR_BIT(GPIO_BASE(GPIO_PORTB) + GPIOx_MODER_OFFSET, MODERy_0_BIT(6));
 
@@ -27,6 +45,7 @@ void init_usart1(void)
 	CLEAR_BIT(GPIO_BASE(GPIO_PORTB) + GPIOx_OTYPER_OFFSET, OTy_BIT(7)); //PB7
 
 	//OSPEEDR led pin = 00 => Low speed
+	// OSPEEDR led pin = 01 => Medium speed
 	SET_BIT(GPIO_BASE(GPIO_PORTB) + GPIOx_OSPEEDR_OFFSET, OSPEEDRy_1_BIT(6)); //PB6
 	CLEAR_BIT(GPIO_BASE(GPIO_PORTB) + GPIOx_OSPEEDR_OFFSET, OSPEEDRy_0_BIT(6));
 
@@ -46,7 +65,7 @@ void init_usart1(void)
 
 	//RCC EN USART1
 	//??????
-	SET_BIT(RCC_BASE + RCC_APB2ENR_OFFSET, USART1EN);
+	SET_BIT(RCC_BASE + RCC_APB2ENR_OFFSET, USART1EN);// APB enable USART
 
 
 
@@ -60,14 +79,14 @@ void init_usart1(void)
 
 	//USART  Configurations
 	//??????
-	SET_BIT(USART1_BASE + USART_CR1_OFFSET, UE_BIT);
-	WRITE_BITS(USART1_BASE + USART_BRR_OFFSET , DIV_MANTISSA_11_BIT , DIV_MANTISSA_0_BIT , DIV_MANTISSA);
-	WRITE_BITS(USART1_BASE + USART_BRR_OFFSET , DIV_FRACTION_3_BIT , DIV_FRACTION_0_BIT , DIV_FRACTION);
+	SET_BIT(USART1_BASE + USART_CR1_OFFSET, UE_BIT);// enable USART
+	WRITE_BITS(USART1_BASE + USART_BRR_OFFSET , DIV_MANTISSA_11_BIT , DIV_MANTISSA_0_BIT , DIV_MANTISSA);// set mantissa for baudrate
+	WRITE_BITS(USART1_BASE + USART_BRR_OFFSET , DIV_FRACTION_3_BIT , DIV_FRACTION_0_BIT , DIV_FRACTION);// set fraction for baudrate
      
 	//UART EN
-	SET_BIT(USART1_BASE + USART_CR1_OFFSET, TE_BIT);
-	SET_BIT(USART1_BASE + USART_CR1_OFFSET, RE_BIT);
-	SET_BIT(USART1_BASE + USART_CR1_OFFSET, RXNEIE_BIT);
+	SET_BIT(USART1_BASE + USART_CR1_OFFSET, TE_BIT);// enable transimitter
+	SET_BIT(USART1_BASE + USART_CR1_OFFSET, RE_BIT);// enable receiver
+	SET_BIT(USART1_BASE + USART_CR1_OFFSET, RXNEIE_BIT);// Set RXNEIE bit in USART_CR1 register to enable RXNE interrupt
 
 	//NVIC Configurations (m+(32*n)) | m=5,n=1
 	SET_BIT(NVIC_ISER_BASE + NVIC_ISERn_OFFSET(1), 5); //IRQ37
@@ -78,10 +97,10 @@ void init_usart1(void)
 
 void usart1_send_char(const char ch)
 {
-	//??????
-	//wait until TXE==1
+	//wait until TXE==1(TXE bit is set)
 	while(!READ_BIT(USART1_BASE + USART_SR_OFFSET,TXE_BIT))
 	;
+	// Write the data to send in the USART_DR register (this clears the TXE bit)
 	REG(USART1_BASE + USART_DR_OFFSET)=ch;
 //    	blink_count(LED_GREEN,1); 
 
@@ -97,7 +116,26 @@ char usart1_receive_char(void)
 
 }
 
-void usart1_handler(void)
+void usart1_handler() {
+	// send a '~' if overrun happens
+	if(READ_BIT(USART1_BASE + USART_SR_OFFSET, ORE_BIT)) {
+		usart1_send_char('~');
+		blink_count(LED_RED, 10);
+	}
+	// When a character is received, the RXNE bit is set
+	else if(READ_BIT(USART1_BASE + USART_SR_OFFSET, RXNE_BIT)) {
+		// Read to the USART_DR register (this clears the RXNE bit)
+		char ch = usart1_receive_char();
+
+		if (ch == '\r')
+			usart1_send_char('\n');
+
+		usart1_send_char(ch);
+	}
+}
+
+
+/*void usart1_handler(void)
 {
         char ch;
 		
@@ -123,7 +161,7 @@ void usart1_handler(void)
         {}
 
 
-}
+}*/
 
 void *_sbrk(int incr)
 {
